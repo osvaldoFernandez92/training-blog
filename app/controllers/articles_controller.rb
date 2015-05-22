be2476c408b8887b60ba0a0cca1c4995f7d3cffd
Osvaldo Fernandez <osvaldo.fernandez@wolox.com.ar>
2015-05-22 13:49:18 -0300

    RSpecs working in the blog. 6/6 specs succeeded. (Including model and controller specs)
    This is a combination of 5 commits.
    
    The first commit's message is:
    branch (Commit used to solve troubles in FactoryGirl.reload)
    
    This is the 2nd commit message:
    spec for ArticlesController created. Context1: User logged in. Context 2: User logged out, redirect. TO SOLVE: FactoryGirl in userLoggedIn and Locale+redirect in usedLoggedOut
    
    This is the 3rd commit message:
    fixes in specs
    
    This is the 4th commit message:
    change the location of factories. Convention over  configuration
    
     This is the 5th commit message:
    specs created. 6/6 specs succeeded

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,14 @@
+# clase ArticlesController
 class ArticlesController < ApplicationController
- before_action :authenticate_user!
+  before_action :authenticate_user!
 
- 	def index
+  def index
     @articles = Article.all
   end
 
-	def new
+  def new
     @article = Article.new
-	end
+  end
 
 	def article
     @article ||= Article.find(params[:id])
@@ -21,13 +22,13 @@ class ArticlesController < ApplicationController
     article
   end
 
-	def create
-  	@article = Article.new(article_params)
- 		 if @article.save
-  		redirect_to @article
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
     else
       render 'new'
-	   end
+    end
   end
 
   def update
@@ -42,12 +43,12 @@ class ArticlesController < ApplicationController
   def destroy
     article
     @article.destroy
-    
     redirect_to articles_path
   end
 
- private
-  	def article_params
-    	params.require(:article).permit(:title, :text)
-  	end
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,21 @@
+# Clase comments controller
 class CommentsController < ApplicationController
+<<<<<<< HEAD
 http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
 	  def create
 
     @comment = article.comments.create(comment_params)
+=======
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+>>>>>>> RSpecs working in the blog. 6/6 specs succeeded. (Including model and controller specs)
     redirect_to article_path(@article)
-  end 
+  end
 
   def destroy
+<<<<<<< HEAD
     @comment = article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
@@ -16,10 +24,17 @@ http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
 
   def article
     @article ||=  Article.find(params[:article_id])
+=======
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+>>>>>>> RSpecs working in the blog. 6/6 specs succeeded. (Including model and controller specs)
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
